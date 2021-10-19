be823245a3510b52eece408e6ed31300622300ff
daniel.seidecaru@lacon.de <daniel.seidecaru@lacon.de>
2021-10-19 11:24:52 +0300

    Section 4 ASP.NET
    ASP.NET section

---
+++ Software Developer  - course/Section 4 - ASP.NET/QuotesApp/Models/Quotes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuotesApp.Models
+{
+    public class Quotes
+    {
+        public int Id { get; set; }
+        public string QuoteAuthor { get; set; }
+        public string QuoteText { get; set; }
+
+
+        public Quotes()
+        {
+
+        }
+    }
+}
---
+++ Software Developer  - course/Section 4 - ASP.NET/QuotesApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuotesApp.Startup))]
+namespace QuotesApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
