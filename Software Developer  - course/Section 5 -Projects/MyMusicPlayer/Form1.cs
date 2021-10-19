e5392a91613a4db1e6904cb76f2d21c8023c89b1
daniel.seidecaru@lacon.de <daniel.seidecaru@lacon.de>
2021-10-19 16:23:51 +0300

    different projects
    different training projects you can find them in the main page

---
+++ Software Developer  - course/Section 5 -Projects/MyMusicPlayer/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyMusicPlayer
+{
+    public partial class MyMusicPlayer : Form
+    {
+        public MyMusicPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+
+            }
+            mPlayer.URL = textBox1.Text;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mPlayer.Ctlcontrols.play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mPlayer.Ctlcontrols.pause();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mPlayer.Ctlcontrols.stop();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
