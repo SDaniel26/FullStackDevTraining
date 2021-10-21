00357915a2bf0aa3dbef656dae892dd7949a5e84
Seidecaru Daniel <63341977+SDaniel26@users.noreply.github.com>
2021-10-21 22:06:51 +0300

    1st steps in python

---
+++ Python Mega Course  Go Beginner to Expert in Python3/coding exercise 3.py
@@ -0,0 +1,15 @@
+A = 50
+B = 75
+
+# Adding two numbers A+B
+result = A + B
+
+# Substracting two numbers B-A
+resultminus = B - A
+
+# printing the values of two Numbers
+print("Result of {0} and {1} is {2}" .format(A, B, result))
+#here we set the format of the numbers as well
+#format() is one of the string formatting methods in Python3, which allows multiple substitutions and value formatting. This method lets us concatenate elements within a string through positional formatting.
+
+print("Rezultatul la {0} si {1} este {2}".format(B,A,resultminus))
